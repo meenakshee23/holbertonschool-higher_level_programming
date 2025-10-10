01fc95f35fa6fe6337e7ee61ef5fccf3cb455b13
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-10 23:15:42 +0400

    new file

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,5 @@ def uniq_add(my_list=[]):
     unique_numbers = set()
     for number in my_list:
         unique_numbers.add(number)
-        total_sum = sum(unique_numbers)
-
+    total_sum = sum(unique_numbers)
     return total_sum
