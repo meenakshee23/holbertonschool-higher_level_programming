6798429a22d7cbec11f2be5a047d31d4f52945f8
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-16 22:50:15 +0400

    new file

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the square with size and position"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
