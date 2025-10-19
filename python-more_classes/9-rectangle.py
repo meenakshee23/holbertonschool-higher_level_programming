c53bc71019f6d39d2fb23fc988ceb2ad9d4058ad
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-19 19:14:01 +0400

    new file

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -78,7 +78,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-      
+
     @classmethod
     def square(cls, size=0):
         """Returns a new Rectangle instance with width == height == size"""
