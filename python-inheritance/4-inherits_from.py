75855ed27da2cb07a4c569379dbce426b67c2eec
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-22 23:30:30 +0400

    new file

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     """returns True if the object is an instance
     of a class that inherited (directly or indirectly)
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
