cf19f3a3cf0cfe4bf0668f025c5f63cf560adf7d
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-23 18:13:13 +0400

    new file

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines an empty class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """The Rectangle class is defined by its width and height"""
+
+    def __init__(self, width, height):
+        """Initializes the width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """The width and the height"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation for the rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
