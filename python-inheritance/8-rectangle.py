34ef9c3348d4e3bf1f321c07bb4ca147ec1c49b3
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-23 17:59:18 +0400

    new file

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
 """This module defines an empty class BaseGeometry"""
-
-
-class BaseGeometry:
-    """Represents an empty class BaseGeometry"""
-
-    def area(self):
-        """Raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Gives  a validate value"""
-        if not isinstance(value, int):
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """The Rectangle class is defined by its width and height"""
@@ -24,13 +9,6 @@ class Rectangle(BaseGeometry):
         """Initializes the width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """The width and the height"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns a string representation for the rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
