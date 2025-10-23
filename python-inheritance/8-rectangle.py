317b5c5ad7dc981cdc4bfc68d49a93050c237c24
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-23 18:00:04 +0400

    new file

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """This module defines an empty class BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """The Rectangle class is defined by its width and height"""
 
@@ -9,6 +10,6 @@ class Rectangle(BaseGeometry):
         """Initializes the width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        
+
         self.__width = width
         self.__height = height
