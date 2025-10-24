0f88dca166720c43862d061242b41e21dc596771
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-24 22:47:15 +0400

    new file

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 from abc import ABC, abstractmethod
-
+"""Abstract Base Classes (ABCs) ensure that derived classes implement specific methods"""
 
 class Animal(ABC):
     @abstractmethod
     def sound(self):
+        """Implement the sound method in each subclass"""
+        pass
+
 
 class Dog(Animal):
     def sound(self):
