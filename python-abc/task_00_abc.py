480bd8bfd38d6431479b2d65e1333cf404a322b7
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-24 14:48:17 +0400

    new file

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
 
 class Dog(Animal):
     def sound(self):
         return "Bark"
-    
+
 class Cat(Animal):
     def sound(self):
         return "Meow"
