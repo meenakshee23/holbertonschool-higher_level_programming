f2dd03460a9020df1fe5061ae32e558697b598b9
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-26 19:37:29 +0400

    new file

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Module defining a FlyingFish class that inherits from Fish and Bird"""
+
+
+class Fish:
+    """Represents a class Fish"""
+
+    def swim(self):
+        """prints a message"""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """Prints a message"""
+        print("The fish lives in water")
+
+
+class Bird:
+    """Represent a class Bird"""
+
+    def fly(self):
+        """prints a message"""
+        print("The bird is flying")
+
+    def habitat(self):
+        """prints a message"""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """Represents a class of Flyingfish from both fish and bird"""
+
+    def fly(self):
+        """Prints a message"""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """prints a message"""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """prints a message"""
+        print("The flying fish lives both in water and the sky")
