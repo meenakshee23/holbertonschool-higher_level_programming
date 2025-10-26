705f3446c8f3853531f04be64f87eec672678c34
meenakshee23 <meenaksheegopaul@gmail.com>
2025-10-26 22:18:57 +0400

    new file

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -7,11 +7,13 @@ import math
 class Shape(ABC):
     """Abstract base class representing a geometric shape"""
     @abstractmethod
+
     def area(self):
         """Calculate and return the area of the shape"""
         pass
 
     @abstractmethod
+
     def perimeter(self):
         """Calculate and return the perimeter of the shape"""
         pass
