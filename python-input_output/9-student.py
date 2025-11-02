82bdc2cd18a199fe5f618c5514aea2635f442049
meenakshee23 <meenaksheegopaul@gmail.com>
2025-11-02 23:04:44 +0400

    new file

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module Write a class Student"""
+
+
+class Student:
+    """Defines a class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student"""
+        return self.__dict__
+    
