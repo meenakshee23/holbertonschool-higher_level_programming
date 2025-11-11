22467afd96b68ee7b21dc9d20182562de56209f6
meenakshee23 <meenaksheegopaul@gmail.com>
2025-11-11 23:58:32 +0400

    new file

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module will explore serialization and
+deserialization using XML as an alternative format to JSON"""
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+    """Serializes the dictionary into XML
+And save it to the given filename"""
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+         child = ET.SubElement(root, key)
+         child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+def deserialize_from_xml(filename):
+     """This will take a filename as its parameter, read the XML data from that file
+     nd return a deserialized Python dictionary"""
+     tree = ET.parse(filename)
+     root = tree.getroot()
+     
+     result = {}
+     
+     for child in root:
+         result[child.tag] = child.text
+
+         return result
