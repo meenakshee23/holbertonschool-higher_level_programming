44e3eb793b44fffac8afb8f2f906fe8d12588f80
meenakshee23 <meenaksheegopaul@gmail.com>
2025-11-20 22:18:05 +0400

    new file

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- A script that prints the following description of the table first_table from the database hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+SHOW CREATE TABLE first_table;
