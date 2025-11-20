0800cea34e0cf9e1dd11f439a40d71f12df530a9
meenakshee23 <meenaksheegopaul@gmail.com>
2025-11-20 22:08:01 +0400

    new file

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- Creates a table called first_table in the current database
+-- id INT, name VARCHAR(256)
+-- If table already exists, the script will not fail
+CREATE TABLE If NOT EXISTS first_table (id INT, name VARCHAR(256));
