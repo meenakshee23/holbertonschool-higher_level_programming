312ee81668a89271aa9ea104b518701eb99536e6
meenakshee23 <meenaksheegopaul@gmail.com>
2025-11-23 23:58:37 +0400

    new file

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Script that creates the table unique_id
+-- id INT with the default value 1 and must be unique0\
+-- name VARCHAR(256)
+-- If the table unique_id already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
