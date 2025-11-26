b37c10fcd97eb385a5f7c94a1f446b81742d421c
meenakshee23 <meenaksheegopaul@gmail.com>
2025-11-27 01:20:10 +0400

    new file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Script that takes in an argument 
-and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""Script that takes in an argument
+and displays all values in the states table of hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
