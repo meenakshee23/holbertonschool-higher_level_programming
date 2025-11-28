3cd72eb41911557eab92a649049b4d1ed64ed5aa
meenakshee23 <meenaksheegopaul@gmail.com>
2025-11-28 14:15:50 +0400

    new file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,7 +4,7 @@ Python file that contains the class definition of a State and
 an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
