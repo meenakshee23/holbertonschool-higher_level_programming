b861b611fd9a6399a05ac256307d93693074d8d7
meenakshee23 <meenaksheegopaul@gmail.com>
2025-12-14 14:07:34 +0400

    new file

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,8 @@
+const addItemDiv = document.querySelector('#add_item');
+const myList = document.querySelector('.my_list');
+
+addItemDiv.addEventListener('click', function () {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Item';
+     myList.appendChild(newItem);
+});
