a20e1153a429346be3b2eacca9e1ab7db110dba1
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-18 16:49:30 +0400

    new file

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for i in range(122, 96, -1):
-    print("%c" % (i if i % 2 == 0 else i - 32), end="")
+for i in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
