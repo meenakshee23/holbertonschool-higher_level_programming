793db8c2850bcb6ce710ba52cfca8e12b5c5b1af
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-21 22:00:17 +0400

    new file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -17,9 +17,14 @@ def roman_to_int(roman_string):
     total = 0
 
     for i in range(len(roman_string)):
-        if i + 1 < len(roman_string) and roman[roman_string[i]] < roman[roman_string[i + 1]]:
-            total -= roman[roman_string[i]]
-        else:
-            total += roman[roman_string[i]]
+        current = roman[roman_string[i]]
+
+        if i + 1 < len(roman_string):
+            next_value = roman[roman_string[i + 1]]
+            if current < next_value:
+                total -= current
+                continue
+
+        total += current
 
     return total
