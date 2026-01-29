c5662a89d9ed4cd681524e25c950f9e059a6a387
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-29 11:54:51 +0400

    new file

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -47,7 +47,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-
-    def area(self):
-        """Return the area of the rectangle"""
-        return self.__width * self.__height
