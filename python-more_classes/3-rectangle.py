57e1e28bc16b84d150eb84e2441abee4226f50e2
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-29 11:56:33 +0400

    new file

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -41,3 +41,13 @@ class Rectangle:
     def area(self):
         """Return the area of the rectangle"""
         return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
