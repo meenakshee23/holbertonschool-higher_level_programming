6d4067a8cb284be6757e022f2829d2369289e797
meenakshee23 <meenaksheegopaul@gmail.com>
2026-01-29 11:28:08 +0400

    new file

---
+++ python-more_classes/3-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+Rectangle = import(‘3-rectangle’).Rectangle
+
+myrectangle = Rectangle(2, 4) print(“Area: {} - Perimeter: {}”.format(myrectangle.area(), my_rectangle.perimeter()))
+
+print(str(myrectangle)) print(repr(myrectangle))
+
+print(“–”)
+
+myrectangle.width = 10 myrectangle.height = 3 print(myrectangle) print(repr(myrectangle))
