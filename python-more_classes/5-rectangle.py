caeecda2f04aeaa5255aac67476e42f9fc60d7d7
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-01 01:06:18 +0400

    new file

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
     def __repr__(self):
         """Return a string representation of the rectangle"""
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         """Print the message"""
         print("Bye rectangle...")
