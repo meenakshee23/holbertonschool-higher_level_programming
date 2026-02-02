677cda684bf21f6f01b78966f5639d2d4d4207ad
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-02 13:34:08 +0400

    new file

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This module write a function that returns
+the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """Returns a list object"""
