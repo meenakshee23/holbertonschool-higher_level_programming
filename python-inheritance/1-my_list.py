a6e0ed7aec85a375b8ff351a995bdb60245f866c
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-03 13:17:13 +0400

    new file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module writes a class MyList that inherits from list"""
+
+
+def print_sorted(self):
+    """Prints the list in sorted ascending order"""
+    print(sorted(self))
