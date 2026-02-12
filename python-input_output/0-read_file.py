9206ae97b7954c48362d837fa7e49bda5057cf54
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-12 23:07:56 +0400

    new file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,6 +4,7 @@ This module writes a function that reads a text file
 (UTF8) and prints it to stdout
 """
 
+
 def read_file(filename=""):
     """Reads a text file"""
     with open(filename, "r", encoding="utf-8") as f:
