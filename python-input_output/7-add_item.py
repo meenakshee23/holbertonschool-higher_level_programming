c553785cd4daa560a06828f1edcdccf99446ea83
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-14 01:05:02 +0400

    new file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module writes a script that adds all arguments
+to a Python list, and then save them to a file
+"""
+
+import sys
+from os import path
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+
+my_list += sys.argv[1:]
+
+save_to_json_file(my_list, filename)
