b9ead305e831000815389e917ccdb5e1c077afae
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-14 01:39:51 +0400

    new file

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module writes a class Student"""
+
+
+class Student:
+    """Represents a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes student with instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return the dictionary representation of the instance"""
+        return self.__dict__
