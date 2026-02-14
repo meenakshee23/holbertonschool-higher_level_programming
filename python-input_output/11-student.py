e8b320d9ea5b7269b45cd0c8671f65c39a6c7c3b
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-14 21:37:47 +0400

    new file

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module writes a class Student"""
+
+
+class Student:
+    """Represents a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes student with instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return the dictionary representation of the instance"""
+        result = {}
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        return self.__dict__.copy()
+    
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
