483795d5a65fa4567c7e892f312370511214cf81
meenakshee23 <meenaksheegopaul@gmail.com>
2026-02-22 21:12:03 +0400

    new file

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module in Python's standard library
+provides basic classes for implementing web server
+"""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class MyHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+if __name__ == "__main__":
+    server = HTTPServer(("", 8000), MyHandler)
+    server.serve_forever()
