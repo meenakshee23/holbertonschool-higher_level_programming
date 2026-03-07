1b391ca2b32993b2c2805a22b35872d7b239367d
meenakshee23 <meenaksheegopaul@gmail.com>
2026-03-07 04:51:14 +0400

    SQL

---
+++ python-object_relational_mapping/ 2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cur = db.cursor()
+    cur.execute(query)
+    query = (
+        "SELECT * FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY id ASC"
+        ).format(sys.argv[4])
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
