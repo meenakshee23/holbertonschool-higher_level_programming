8e772939b5f785a76e80e515d10d80dac2aceb9a
meenakshee23 <meenaksheegopaul@gmail.com>
2026-03-29 18:16:02 +0400

    JS

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+const redHeader = document.getElementById('red_header');
+const header = document.querySelector('header');
+
+redHeader.addEventListener('click', () => {
+  header.classList.add('red');
+});
