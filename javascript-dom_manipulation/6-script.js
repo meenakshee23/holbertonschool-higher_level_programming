3b9705cb2125db7ed4ca2884bcf37a6893c15804
meenakshee23 <meenaksheegopaul@gmail.com>
2026-04-02 23:53:31 +0400

    CSS

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,10 +1,14 @@
 const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+const characterEl = document.getElementById('character');
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('character').textContent = data.name;
-  })
-  .catch(error => {
+async function fetchCharacter() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    characterEl.textContent = data.name;
+  } catch (error) {
     console.error('Error fetching character:', error);
-  });
+  }
+}
+
+fetchCharacter();
