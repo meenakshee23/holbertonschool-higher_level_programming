1d4f525b981e1a6acc312e32ee176df7b1049a7f
meenakshee23 <meenaksheegopaul@gmail.com>
2026-04-04 23:21:41 +0400

    update

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,24 +1,75 @@
+#!/usr/bin/python3
+"""
+Flask app that displays products from JSON, CSV, or SQLite
+"""
+
+from flask import Flask, render_template, request
+import json
+import csv
 import sqlite3
 
-def create_database():
-       conn = sqlite3.connect('products.db')
-       cursor = conn.cursor()
-       cursor.execute('''
-           CREATE TABLE IF NOT EXISTS Products (
-               id INTEGER PRIMARY KEY,
-               name TEXT NOT NULL,
-               category TEXT NOT NULL,
-               price REAL NOT NULL
-           )
-       ''')
-       cursor.execute('''
-           INSERT INTO Products (id, name, category, price)
-           VALUES
-           (1, 'Laptop', 'Electronics', 799.99),
-           (2, 'Coffee Mug', 'Home Goods', 15.99)
-       ''')
-       conn.commit()
-       conn.close()
+app = Flask(__name__)
+
+def get_json_data():
+    try:
+        with open('products.json', 'r') as f:
+            return json.load(f)
+    except Exception:
+        return None
+
+def get_csv_data():
+    try:
+        data = []
+        with open('products.csv', newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                row['price'] = float(row['price'])
+                data.append(row)
+        return data
+    except Exception:
+        return None
+
+def get_sql_data():
+    try:
+        conn = sqlite3.connect('products.db')
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT id, name, category, price FROM Products")
+        rows = cursor.fetchall()
+
+        conn.close()
+
+        data = []
+        for row in rows:
+            data.append({
+                "id": row[0],
+                "name": row[1],
+                "category": row[2],
+                "price": row[3]
+            })
+
+        return data
+
+    except Exception:
+        return None
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+
+    if source == 'json':
+        products = get_json_data()
+    elif source == 'csv':
+        products = get_csv_data()
+    elif source == 'sql':
+        products = get_sql_data()
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if products is None:
+        return render_template('product_display.html', error="Error loading data")
+
+    return render_template('product_display.html', products=products)
 
 if __name__ == '__main__':
-       create_database()
+    app.run(port=5000)
